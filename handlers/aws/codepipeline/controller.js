7a657050346ce4ace218dc1723ff36ed2207d3e2
Jose Antonio <josegrobles1997@gmail.com>
2018-07-25 13:57:57 +0200

    Fixed Slack imports

--- handlers/aws/codepipeline/controller.js
+++ handlers/aws/codepipeline/controller.js
@@ -1,3 +1,3 @@
-const slack = require('../libs/slack/index');
+const slack = require('../../libs/slack/index');
 
 module.exports.sendNotification = attachments => slack.postMessage(attachments, slack.router(''));
